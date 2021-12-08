96f746e6f0999a03387dc64f6be953e82954beeb
ando-hunterz <ando.hunterz@gmail.com>
2021-12-08 16:59:25 +0700

    add docker, edit field request

--- requests/roomRequest.js
+++ requests/roomRequest.js
@@ -1,11 +1,11 @@
 const { body } = require("express-validator")
-const { Room, User } = require('../models/index');
+const { Room, User, Node } = require('../models/index');
 
 let roomRequest = () => {
     return [
         body('name')
             .not().isEmpty().withMessage('Name Field is Required').bail()
-            .isAlpha().withMessage('Must Be Alpha Name').bail()
+            .isAlpha(undefined,{ignore: ' -_'}).withMessage('Must Be Alpha Name').bail()
             .custom(async (value, {req}) => {
                 let room = await Room.findAll({where: {name: value}})
                 if(room.length > 0) {
@@ -21,6 +21,9 @@ let roomRequest = () => {
             .not().isEmpty().withMessage('NodeId is Required').bail()
             .isNumeric().withMessage('Must Be Numeric Value').bail()
             .custom(async (value, {req}) => {
+                let node = await Node.findAll({where: {id: value}})
+                console.log(node)
+                if(node.length == 0) return Promise.reject('No Node Found')
                 let room = await Room.findAll({where: {NodeId: value}})
                 if(room.length > 0) {
                     if(room[0].id != req.params.id) return Promise.reject('Room Found')
