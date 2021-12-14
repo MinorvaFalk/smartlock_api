aad3c7b1921bba228a0a15ae0a0533065b1ec57e
ando-hunterz <ando.hunterz@gmail.com>
2021-12-14 20:49:05 +0700

    add booking request (untested)

--- requests/bookingRequest.js
+++ requests/bookingRequest.js
@@ -20,6 +20,17 @@ let bookingRequest = () => {
                 if(!user.length > 0) return Promise.reject('nim is not found')
             }),
 
+        body('participant')
+            .not().isEmpty().withMessage('Participant is Required').bail()
+            .custom(async value => {
+                let notId;
+                value.forEach((item) => {
+                    let user = await User.findOne({where: {uid: item}}),
+                    if (user == null) notId == true;
+                })
+                if(!notId) return Promise.reject("UID Not Found")
+            }),
+
         body('start_date')
             .not().isEmpty().withMessage('Start Date Field is Required').bail()
             .custom(value => {
